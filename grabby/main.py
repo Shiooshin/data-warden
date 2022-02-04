4b344ad16556650b3cec5270bce2b12a39d51297
mfaryna <marikfarik@gmail.com>
2022-02-04 23:56:55 +0200

    more data from github API

--- grabby/main.py
+++ grabby/main.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://api.github.com/search/repositories?q=topic:big-data&sort=stars"
+url = "https://api.github.com/search/repositories?q=topic:big-data&sort=stars&per_page=100"
 headers = {"Accept": "applicaiton/vnd.github.v3+json"}
 
 r = requests.get(url, headers=headers)
@@ -10,10 +10,11 @@ response_dict = r.json()
 print(f"Total repos: {response_dict['total_count']}")
 
 repo_dicts = response_dict["items"]
-for repo in repo_dicts:
+print(f"Page repos: {len(repo_dicts)}")
+for repo in repo_dicts[:5]:  # first 5 elements for testing
     print(f'repo name: {repo["name"]}')
     print(f'stars: {repo["stargazers_count"]}')
-    print(f'language: {repo["language"]}')
-
-    for item in repo["topics"]:
-        print(f'topic: {item}')
+    print(f'watches: {repo["watchers_count"]}')
+    print(f'open_issues_count: {repo["open_issues_count"]}')
+    print(f'topics: {repo["topics"]}')
+    print(f'forks_count: {repo["forks_count"]}')
